8ec9d6900be1fe154284f4cce750d4722d0aabf6
Saif Uddin <admin@saifuddin.uk>
2024-02-06 23:09:07 +0000

    tidied code

--- assets/src/App.jsx
+++ assets/src/App.jsx
@@ -1,8 +1,8 @@
 // import { useState } from 'react'
 import "./App.css";
 import React from 'react';
-import Header from "./components/Header";
 import Wrapper from "./components/Wrapper"
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { BrowserRouter as Router } from 'react-router-dom';
 
