c4adfc6f0097742b2b46883da109880a59704767
Saif Uddin <admin@saifuddin.uk>
2024-02-22 16:21:18 +0000

    testing with inset css property instead of top, bottom, left, right

--- assets/src/pages/Home.jsx
+++ assets/src/pages/Home.jsx
@@ -1,8 +1,30 @@
 import React from "react";
 import NavButton from "../components/NavButton";
-import Hero from "../components/Hero";
 
 function Home (){
+
+    const logoStyle = {
+        position: 'absolute',
+        border: '1px black solid',
+        display: 'inline-block ',
+        textAlign: 'center',
+        top:'45%',
+        left: '45%',
+        color: 'black',
+    }
+
+    const testStyle = (posArray) => {
+
+        return {
+            position: 'absolute',
+            top: posArray.top,
+            right: posArray.right,
+            bottom: posArray.bottom,
+            left: posArray.left,
+            inset: `${posArray.yAxis} ${posArray.xAxis}`,
+            textAlign: 'center',
+            width: '100%',
+    }}
 
     const NavBtnArray = [
         ["About", "About"],
@@ -10,48 +32,37 @@ function Home (){
         ["Contact", "Contact"],
         ["CV", "CV"],
     ]
-
-    const logoStyle = {
-        position: 'relative',
-        border: '1px black solid',
-        display: 'inline-block ',
-        textAlign: 'center',
-        top:'30vh',
-        left: '10vw',
-        color: 'black',
-    }
-
-    const testStyle = (posArray) => {
-        
-        return {
-            position: 'absolute',
-            top: posArray.top,
-            right: posArray.right,
-            bottom: posArray.bottom,
-            left: posArray.left,
-            textAlign: 'center',
-            width: '100%',
-    }}
     
     return (<>
-        <Hero /> 
         HOME
-        World icon with nav buttons around icon image
-        
-        { 
-        NavBtnArray.map((btn)=>{
-            return (<NavButton
-            key={`${btn[1]}-link`}
-            id={`${btn[1]}-link`}
-            nav={btn[0]}
-            text={btn[1]}
-            />
-        )})}
+        World icon with nav buttons around
+        {
+            NavBtnArray.map((btn)=>(
+                <NavButton
+                    key={`${btn[0]}-link`}
+                    id={`${btn[0]}-link`}
+                    text={btn[0]}
+                    nav={btn[1]}
+                />
+            ))
+        }
         <h3 style={logoStyle}>
             LOGO
             <div
-            style={testStyle({top:'-4rem',right:'',bottom:'',left:''})}>
+            style={testStyle({yAxis:'-4rem', xAxis:'0'})}>
                 1
+            </div>
+            <div
+            style={testStyle({yAxis:'0', xAxis:'4rem'})}>
+                3
+            </div>
+            <div
+            style={testStyle({yAxis:'4rem', xAxis:'0'})}>
+                6
+            </div>
+            <div
+            style={testStyle({yAxis:'0', xAxis:'-4rem'})}>
+                9
             </div>
         </h3>
     </>)
